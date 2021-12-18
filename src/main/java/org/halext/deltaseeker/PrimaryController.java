71172a836a62a0938734d896cc4751ac00df853c
Justin Scofield <justin.scofield@outlook.com>
2021-12-18 12:40:54 -0500

    Insert GUI control for streaming service and client as well as additional websocket dependencies in pom

--- src/main/java/org/halext/deltaseeker/PrimaryController.java
+++ src/main/java/org/halext/deltaseeker/PrimaryController.java
@@ -2,11 +2,16 @@ package org.halext.deltaseeker;
 
 import java.io.IOException;
 
+import javax.websocket.DeploymentException;
+
 import org.halext.deltaseeker.service.Client;
+import org.json.simple.parser.ParseException;
 
 import javafx.fxml.FXML;
 
 public class PrimaryController {
+
+    Client client = new Client();
 
     @FXML
     private void switchToTrainingMode() throws IOException {
@@ -15,8 +20,12 @@ public class PrimaryController {
 
     @FXML
     private void postAccessToken() throws IOException, org.json.simple.parser.ParseException {
-        Client client = new Client();
         client.retrieveKeyFile();
         client.postAccessToken();
+    }
+
+    @FXML
+    private void startStreamingSession() throws IOException, ParseException, DeploymentException, java.text.ParseException {
+        client.openStream();
     }
 }
